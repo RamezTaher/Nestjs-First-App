f1c60b6f37ea47b2d33f15db2861f2d40574e985
Ramez Taher <ramezgamer11@gmail.com>
2022-11-25 10:54:04 +0100

    first basic get controller

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -4,6 +4,30 @@ import { Controller, Get } from '@nestjs/common';
 export class UsersController {
   @Get()
   getUsers() {
-    return { username: 'Ramez', email: 'rameztaher2002@gmail.com' };
+    return [{ username: 'Ramez', email: 'rameztaher2002@gmail.com' }];
+  }
+
+  @Get('posts')
+  getUsersPosts() {
+    return [
+      {
+        username: 'Ramez',
+        email: 'rameztaher2002@gmail.com',
+        posts: [
+          { id: 2, title: 'nest js post' },
+          { id: 1, title: 'nest js post' },
+          { id: 3, title: 'nest js post' },
+        ],
+      },
+    ];
+  }
+
+  @Get('posts/comments')
+  getUsersPostsComments() {
+    return {
+      id: 1,
+      title: 'nestjs post',
+      comments: ['bla blabla', 'blablabla'],
+    };
   }
 }
