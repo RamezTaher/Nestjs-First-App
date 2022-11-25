78bd165151cacbba4c2bbf3a4ba2f25b1c179dfa
Ramez Taher <ramezgamer11@gmail.com>
2022-11-25 10:45:51 +0100

    first controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [UsersModule],
   providers: [],
 })
 export class AppModule {}
