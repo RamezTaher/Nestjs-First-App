9014b680f5173c991adb1e35aff14c0f1f50ddee
Ramez Taher <ramezgamer11@gmail.com>
2022-11-25 14:43:12 +0100

    req body and dto

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
@@ -33,8 +33,8 @@ export class UsersController {
   }
 
   @Post()
-  createUser(@Req() request: Request, @Res() response: Response) {
-    console.log(request.body);
-    response.send('Created');
+  createUser(@Body() userInfo: CreateUserDto) {
+    console.log(userInfo);
+    return {};
   }
 }
