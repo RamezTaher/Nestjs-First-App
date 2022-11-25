bbf6d80ec3257ed5a0b6557b1b7b9c1109474bc6
Ramez Taher <ramezgamer11@gmail.com>
2022-11-25 14:27:47 +0100

    fiest post request

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -29,5 +30,11 @@ export class UsersController {
       title: 'nestjs post',
       comments: ['bla blabla', 'blablabla'],
     };
+  }
+
+  @Post()
+  createUser(@Req() request: Request, @Res() response: Response) {
+    console.log(request.body);
+    response.send('Created');
   }
 }
